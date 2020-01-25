32fcaa7032094eac6035d55b744782a40e559f1e
Yaniv Silberman <yanivsilberman@gmail.com>
2020-01-25 11:10:51 -0500

    config for background and content scripts

---
+++ src/contentscript/contentscript.ts
@@ -0,0 +1,13 @@
+// import './contentscript.scss';
+// import axios from 'axios';
+
+// const endpoint = 'https://0cd9b92c.ngrok.io/titties';
+
+const isThisContentscript = true;
+console.log('isThisContentscript', isThisContentscript);
+// if (isThisContentscript) {
+//   axios
+//     .get(endpoint)
+//     .then(res => console.log({ res }))
+//     .catch(err => console.log({ err }));
+// }
