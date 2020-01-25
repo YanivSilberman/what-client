e4d6ee347d00c393fcaea65e648f89d65ce42cc9
Yaniv Silberman <yanivsilberman@gmail.com>
2020-01-25 14:23:27 -0500

    added react popup and text scraping works

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,39 +1,60 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import * as $ from 'jquery';
 import './contentscript.scss';
 
-const endpoint = process.env.API_URL;
+// const isThisContentscript = true;
 
-const isThisContentscript = true;
-
-function getTextForElement(el: string) {
-  let content = '';
-  const allSpans = document.getElementsByTagName(el);
+// function getTextForElement(el: string) {
+//   let content = '';
+//   const allSpans = document.getElementsByTagName(el);
   
-  for(var i = 0; i < allSpans.length; i ++) {
-    content = content + allSpans[i].textContent;
-  }
+//   for(var i = 0; i < allSpans.length; i ++) {
+//     content = content + allSpans[i].textContent + '. ';
+//   }
 
-  return content;
-}
+//   return content;
+// }
 
-if (isThisContentscript) {
-  // const content = document.body.textContent
-  //   .trim()
-  //   .replace(/(\r\n|\n|\r)/gm, "") // remove escape chars
-  //   // .match( /[^\.!\?]+[\.!\?]+/g ) // array of sentences;
-  //   // .filter(s => {
-  //   //   const count = countWords(s);
-  //   //   return count > 6 && count < 15;
-  //   // })
+// function setBack(from: string, to: string) {
+//   console.log('runs');
+//   const el = $(`a.what-selection:contains('${from}')`);
+//   const replacement = el.html()
+//     .replace(from, to);
+//   el.html(replacement);
+// }
 
-  const content = [
-    'span', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
-  ].map(i => getTextForElement(i)).join(". ");
+// function replaceElement(from: string, to: string) {
+//   const el = $(`:contains('${from}')`);
+//   const setBack = `onClick="LogIt('big test')"`;
+//   const replacement = el.html()
+//     .replace(from, `<a class="what-selection" ${setBack}>${to}</a>`);
+//   el.html(replacement);
+// }
 
-  axios
-    .post(endpoint, {
-      text: content
-    })
-    .then(res => console.log({ res }))
-    .catch(err => console.log({ err }));
-}
+// if (isThisContentscript) {
+//   // const content = document.body.textContent
+//   //   .trim()
+//   //   .replace(/(\r\n|\n|\r)/gm, "") // remove escape chars
+//   //   // .match( /[^\.!\?]+[\.!\?]+/g ) // array of sentences;
+//   //   // .filter(s => {
+//   //   //   const count = countWords(s);
+//   //   //   return count > 6 && count < 15;
+//   //   // })
+
+//   // replaceElement('discover', 'still works');
+  
+//   const content = [
+//     'span', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+//   ].map(i => getTextForElement(i)).join(". ");
+
+//   axios
+//     .post(process.env.API_URL, {
+//       text: content
+//     })
+//     .then(res => console.log({ res }))
+//     .catch(err => console.log({ err }));
+// }
+
+// // const script = document.createElement('script').text = 
+// //   'function LogIt(msg) { console.log(msg);}';
+// // document.body.appendChild(script as any);
